52cea0536f9dba1cf2454dfc0064ce3c6ccc306e
zhechkoz <jechkoj@gmail.com>
2017-05-15 14:51:22 +0200

    reset InterestingProgram

--- phase1/dyninsttool/src/InterestingProgram.c
+++ phase1/dyninsttool/src/InterestingProgram.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
 
-void print(char *message)
-{
+void print(char *message) {
     printf("%s\n", message);
 }
 
-
-void InterestingProcedure()
-{
+void InterestingProcedure() {
     printf("\t This is an interesting procedure\n");
 }
 
-int main()
-{
+int main() {
     printf("Hello, world!\n");
 
     int i; 
