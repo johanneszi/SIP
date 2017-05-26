465679804e2ecf6b411ca89a0b3961c0660d9818
Johannes Zirngibl <johannes.zirngibl@t-online.de>
2017-05-26 15:43:21 +0200

    implements callgraph

--- phase2/llvm-pass-skeleton/skeleton/Skeleton.cpp
+++ phase2/llvm-pass-skeleton/skeleton/Skeleton.cpp
@@ -10,8 +10,6 @@
 #include "llvm/Analysis/CallGraph.h"
 #include "llvm/Analysis/CallGraphSCCPass.h"
 
-#include "llvm/Transforms/IPO/PassManagerBuilder.h"
-#include "llvm/Transforms/IPO/PassManagerBuilder.h"
 #include "llvm/Transforms/Utils/BasicBlockUtils.h"
 
 #include "llvm/Support/raw_ostream.h"
@@ -19,10 +17,12 @@
 
 #include <string>
 #include <map>
+#include <algorithm>
 
 using namespace llvm;
 
 const std::string CHECKFUNC = "check";
+const std::vector<std::string> ENTRYPOINTS = {"main"};
 
 namespace {
   struct SkeletonPass : public ModulePass {
@@ -82,64 +82,57 @@ namespace {
 	    	return false;
 	    }
 	  	
-	  	unsigned sccNum = 0;
-  		errs() << "SCCs for the program in PostOrder:";
-  		
-  		for (scc_iterator<CallGraph*> SCCI = scc_begin(CG); !SCCI.isAtEnd(); ++SCCI) {
-    		const std::vector<CallGraphNode*> &nextSCC = *SCCI;
-    		errs() << "\nSCC #" << ++sccNum << " : ";
-    
-    		for (std::vector<CallGraphNode*>::const_iterator I = nextSCC.begin(), E = nextSCC.end(); I != E; ++I) {
-      			Function *func = (*I)->getFunction();
-      			
-      			if (func != nullptr) {
-      				errs() << func->getName() << ", ";;
-      			} else {
-      				errs() << "external node" << ", ";
-      			}
-      		}
-  		}
-  		
-  		errs() << "\n";
-	  	
-	  	/*
-		for (auto i = CG->begin(), ie = CG->end(); i != ie; i++) {
-			const Function *func = (*i).first;
-			
-			if (func != nullptr && func->size() != 0) {
-				errs()<<"Function " << func->getName() << "\n";
-			} else {
-				errs()<<"Function is null or size is 0\n";
-			}
-			
-			CallGraphNode *node = (*i).second.get();
-			Function *func2 = node->getFunction();
-			errs()<<node->size();
-			if (func2 != nullptr) {
-				errs()<<"Function2 is not null ";
-				errs()<<"and size is: " << func2->size()  << "\n";
-				if(func2->size() != 0) {
-					errs()<<"Function2 " << func->getName() << "\n";
-				}
-			} else {
-				errs()<<"Function is null\n";
-			}
-			
-			
-		}
-		
-      	for(auto &i : g) {
-      		if (i.first != NULL) {
-   				errs()<<"Function " << i.first->getName() << " references:\n";
-      			for (const auto &j : *(i.second)) {
-      				if (j.second != nullptr) {
-      					errs()<<"\t"<<j.second->getFunction()->getName() << "\n";
-      				}
-      			}
-      		}
-      	} */
+	  	getCallGraphForFunction(CG, "InterestingProcedure");
       	
       	return false;		
+    }
+    
+    std::vector<std::vector<std::string> > getCall(CallGraph *CG, StringRef func, std::vector<std::string> seen) {
+    	std::vector<std::vector<std::string> > calls;
+    	std::vector<std::vector<std::string> > results;
+    	
+    	if(std::find(ENTRYPOINTS.begin(), ENTRYPOINTS.end(), func) != ENTRYPOINTS.end()) {
+    		std::vector<std::string> call;
+    		call.push_back(func);
+    		calls.push_back(call);
+    		
+    		return calls;
+    	}
+    	
+    	for (const auto &caller : *CG) {
+    		const Function *callingFunction = caller.first;
+    		if (callingFunction == nullptr) { continue; }
+			
+    		for (const auto &callee : *(caller.second.get())) {
+    			Function *calledFunction = callee.second->getFunction();
+    			
+    			if (calledFunction != nullptr && calledFunction->size() != 0 && calledFunction->getName() == func) {
+					if(std::find(seen.begin(), seen.end(), callingFunction->getName()) == seen.end()) {
+						seen.push_back(callingFunction->getName());
+						std::vector<std::vector<std::string> > oldCalls = getCall(CG, callingFunction->getName(), seen);				
+						calls.insert(std::end(calls), std::begin(oldCalls), std::end(oldCalls));
+					}
+    			}
+    		}
+    	}
+    	
+    	for (auto vec : calls) {
+    		vec.push_back(func);
+    		results.push_back(vec);
+    	}
+    	
+    	return results;
+    }
+    
+    void getCallGraphForFunction(CallGraph *CG, StringRef func) {
+    	std::vector<std::vector<std::string> > vec = getCall(CG, func, std::vector<std::string>());
+    	for (auto vec1 : vec) {
+    		for(auto val : vec1) {
+    			errs() << val << " -> ";
+    		}	
+   			errs()<<"\n";	
+    	}
+    	
     }
     
     virtual bool doFinalization(Module &M) {
@@ -160,13 +153,5 @@ namespace {
 
 char SkeletonPass::ID = 0;
 
-// Automatically enable the pass.
-// http://adriansampson.net/blog/clangpass.html
-static void registerSkeletonPass(const PassManagerBuilder &,
-                         legacy::PassManagerBase &PM) {
-  	PM.add(new SkeletonPass());
-}
-static RegisterStandardPasses
-  RegisterMyPass(PassManagerBuilder::EP_EnabledOnOptLevel0,
-                 registerSkeletonPass);
+RegisterPass<SkeletonPass> X("skeleton", "Skeleton Pass", false, false);
          
