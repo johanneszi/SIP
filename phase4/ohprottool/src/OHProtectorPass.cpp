34307c75111b629642bb91535b7a02222e77ae2e
Zhechko Zhechev <jechkoj@gmail.com>
2017-06-19 18:05:46 +0200

    implements hash variables injection

--- phase4/ohprottool/src/OHProtectorPass.cpp
+++ phase4/ohprottool/src/OHProtectorPass.cpp
@@ -13,6 +13,8 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <random>
+#include <ctime>
 
 #include <json/value.h>
 #include <json/reader.h>
@@ -26,12 +28,21 @@ using std::vector;
 using std::string;
 
 namespace {
+    const static vector<unsigned> PROTECTEDINSTRUCTIONS = {Instruction::Load, Instruction::Store,
+                                                           Instruction::ICmp, Instruction::Sub,
+                                                           Instruction::Add};
+
     struct OHProtectorPass : public ModulePass {
         // Variables
         static char ID;
+        int numHashVariables = 5;
         bool verbose = false;
 
-        Type *boolTy, *voidTy;
+        Type *boolTy, *voidTy, *int32Ty;
+        vector<Instruction *> instToProtect, instToObfuscate;
+        vector<GlobalVariable *> globals;
+
+        std::random_device rd;
 
         OHProtectorPass() : ModulePass(ID) {}
 
@@ -39,6 +50,13 @@ namespace {
         bool doInitialization(Module &M) override;
         bool runOnModule(Module &M) override;
         void getAnalysisUsage(AnalysisUsage &AU) const override;
+
+        bool shouldProtect(Instruction *instruction);
+        void insertGlobals(Module &M, int numHashVars);
+        void insertProtection(Module &M, vector<Instruction *> instuctions, bool finalRun);
+        BinaryOperator* generateHashFunction(IRBuilder<> *builder, Value *operandOne, Value *operandTwo);
+        template<typename T>
+        vector<T *> twistGetPartFromVector(vector<T *> input, double procent);
     };
 
     bool OHProtectorPass::doInitialization(Module &M) {
@@ -46,6 +64,9 @@ namespace {
 
         boolTy = Type::getInt1Ty(ctx);
         voidTy = Type::getVoidTy(ctx);
+        int32Ty = Type::getInt32Ty(ctx);
+
+        srand(time(0));
 
         return false;
     }
@@ -58,12 +79,9 @@ namespace {
     bool OHProtectorPass::runOnModule(Module &M) {
         const auto &input_dependency_info = getAnalysis<input_dependency::InputDependencyAnalysis>();
 
+        insertGlobals(M, numHashVariables);
 
-        LLVMContext &ctx = M.getContext();
-        IRBuilder<> builder(ctx);
-        Constant *printmeCond = M.getOrInsertFunction("printmeCond",
-                                        FunctionType::get(Type::getInt32Ty(ctx), Type::getInt32Ty(ctx), false));
-
+        // Save all input independent instructions which has to be protected
         for (auto &F : M) {
             // No input dependency info for declarations
             if (F.isDeclaration()) {
@@ -72,81 +90,184 @@ namespace {
 
             for (auto &B : F) {
                 for (auto &I : B) {
-                    if (input_dependency_info.isInputDependent(&I)) {
-                        continue;
-                    }
-
-                    errs() << "Instruction: " << I << " is input independent\n";
-                    switch (I.getOpcode()) {
-                        case Instruction::Load: {
-                            //errs() << I.getOpcodeName() << "\n";
-                            auto *instruction = dyn_cast<LoadInst>(&I);
-                            Value *memory = instruction->getPointerOperand();
-
-                            Type *type = memory->getType();
-                            Type *content = type->getContainedType(0);
-
-                            if(content->isPointerTy()) {
-                                continue;
-                            }
-
-                            Constant *printme = M.getOrInsertFunction("printme",
-                                                        FunctionType::get(Type::getVoidTy(ctx), Type::getInt32Ty(ctx), false));
-
-                            //IRBuilder<> builder(I.getNextNode());
-                            //memory = builder.CreatePtrToInt(memory, Type::getInt32Ty(ctx));
-                            //builder.CreateCall(printme, memory);
-
-                            break;
-                        }
-                        case Instruction::Store: {
-                            //errs() << I.getOpcodeName() << "\n";
-                            break;
-                        }
-                        case Instruction::ICmp: {
-                            //errs() << I.getOpcodeName() << "\n";
-
-                            auto *instruction = dyn_cast<ICmpInst>(&I);
-
-
-                            builder.SetInsertPoint(I.getNextNode());
-                            //IRBuilder<> builder(I.getNextNode());
-
-                            //builder.CreateCall(printmeCond, hash);
-
-                            break;
-                        }
-                        case Instruction::Br: {
-                            //errs() << I.getOpcodeName() << "\n";
-                            auto *instruction = dyn_cast<BranchInst>(&I);
-                            if (instruction->isConditional()) {
-                                Value *cond = instruction->getCondition();
-
-                                Constant *printmeCond = M.getOrInsertFunction("printmeCond",
-                                                            FunctionType::get(Type::getVoidTy(ctx), Type::getInt64Ty(ctx), false));
-
-
-                                //IRBuilder<> builder(I.getNextNode());
-
-                                //builder.CreateCall(printmeCond, cond);
-
-                            }
-
-                            break;
-                        }
-                        case Instruction::Add: {
-                            //errs() << I.getOpcodeName() << "\n";
-                            break;
-                        }
-
-                        default:
-                            break;
+                    if (shouldProtect(&I) && !input_dependency_info.isInputDependent(&I)) {
+                        instToProtect.push_back(&I);
                     }
                 }
             }
         }
 
-        return false;
+        insertProtection(M, instToProtect, false);
+
+        vector<Instruction *> instToObfuscatePart = twistGetPartFromVector(instToObfuscate, 0.2);
+        instToObfuscate.clear();
+
+        insertProtection(M, instToObfuscatePart, true);
+
+        return true;
+    }
+
+    template<typename T>
+    vector<T *> OHProtectorPass::twistGetPartFromVector(vector<T *> input, double procent) {
+        std::mt19937 twister(rd());
+        std::shuffle(input.begin(), input.end(), twister);
+
+        typename vector<T *>::const_iterator first = input.begin();
+        typename vector<T *>::const_iterator last = input.begin() + int(input.size() * procent);
+        vector<T *> part(first, last);
+
+        return part;
+    }
+
+    bool OHProtectorPass::shouldProtect(Instruction *instruction) {
+        unsigned opCode = instruction->getOpcode();
+        return std::find(PROTECTEDINSTRUCTIONS.begin(), PROTECTEDINSTRUCTIONS.end(), opCode) != PROTECTEDINSTRUCTIONS.end();
+    }
+
+    BinaryOperator* OHProtectorPass::generateHashFunction(IRBuilder<> *builder, Value *operandOne, Value *operandTwo) {
+        int randNum = rand() % 2;
+
+        if (randNum == 0) {
+            return dyn_cast<BinaryOperator>(builder->CreateAdd(operandOne, operandTwo));
+        }
+
+        return dyn_cast<BinaryOperator>(builder->CreateXor(operandOne, operandTwo));
+    }
+
+    void OHProtectorPass::insertProtection(Module &M, vector<Instruction *> instuctions, bool finalRun) {
+        LLVMContext &ctx = M.getContext();
+        IRBuilder<> builder(ctx);
+
+        unsigned int globalsIndex = 0;
+        std::mt19937 twister(rd());
+
+        for (auto *I : instuctions) {
+
+            switch (I->getOpcode()) {
+                case Instruction::Load: {
+                    auto *instruction = dyn_cast<LoadInst>(I);
+
+                    Value *memory = instruction->getPointerOperand();
+                    Type *type = memory->getType();
+                    Type *content = type->getContainedType(0);
+
+                    if(content->isPointerTy()) {
+                        continue;
+                    }
+
+                    builder.SetInsertPoint(instruction->getNextNode());
+
+                    LoadInst *loadGlobal = builder.CreateLoad(globals[globalsIndex]);
+                    Value *casted = CastInst::CreateIntegerCast(instruction, int32Ty, false, "", loadGlobal->getNextNode());
+                    BinaryOperator *hash = generateHashFunction(&builder, casted, loadGlobal);
+                    StoreInst *storeGlobal = builder.CreateStore(hash, globals[globalsIndex]);
+
+                    if (!finalRun) {
+                        instToObfuscate.push_back(hash);
+                        instToObfuscate.push_back(loadGlobal);
+                        instToObfuscate.push_back(storeGlobal);
+                    }
+
+                    break;
+                }
+                case Instruction::Store: {
+                    auto *instruction = dyn_cast<StoreInst>(I);
+
+                    Value *memory = instruction->getPointerOperand();
+                    Type *type = memory->getType();
+                    Type *content = type->getContainedType(0);
+
+                    if(content->isPointerTy()) {
+                        continue;
+                    }
+
+                    builder.SetInsertPoint(instruction->getNextNode());
+
+                    LoadInst *loadValue = builder.CreateLoad(memory);
+                    LoadInst *loadGlobal = builder.CreateLoad(globals[globalsIndex]);
+                    Value *casted = CastInst::CreateIntegerCast(loadValue, int32Ty, false, "", loadGlobal->getNextNode());
+
+                    BinaryOperator *hash = generateHashFunction(&builder, casted, loadGlobal);
+                    StoreInst *storeGlobal = builder.CreateStore(hash, globals[globalsIndex]);
+
+                    if (!finalRun) {
+                        instToObfuscate.push_back(hash);
+                        instToObfuscate.push_back(loadGlobal);
+                        instToObfuscate.push_back(storeGlobal);
+                    }
+
+                    break;
+                }
+                case Instruction::ICmp: {
+                    auto *instruction = dyn_cast<ICmpInst>(I);
+
+                    builder.SetInsertPoint(instruction->getNextNode());
+
+                    LoadInst *loadGlobal = builder.CreateLoad(globals[globalsIndex]);
+                    Value *casted = CastInst::CreateIntegerCast(instruction, int32Ty, false, "", loadGlobal->getNextNode());
+                    BinaryOperator *hash = generateHashFunction(&builder, casted, loadGlobal);
+                    StoreInst *storeGlobal = builder.CreateStore(hash, globals[globalsIndex]);
+
+                    if (!finalRun) {
+                        instToObfuscate.push_back(hash);
+                        instToObfuscate.push_back(loadGlobal);
+                        instToObfuscate.push_back(storeGlobal);
+                    }
+
+                    break;
+                }
+                case Instruction::Sub: case Instruction::Add: {
+                    auto *instruction = dyn_cast<BinaryOperator>(I);
+
+                    builder.SetInsertPoint(instruction->getNextNode());
+
+                    LoadInst *loadGlobal = builder.CreateLoad(globals[globalsIndex]);
+
+                    Value *firstOperand = instruction->getOperand(0);
+                    firstOperand = CastInst::CreateIntegerCast(firstOperand, int32Ty, false, "", loadGlobal->getNextNode());
+                    Value *secondOperand = instruction->getOperand(1);
+                    secondOperand = CastInst::CreateIntegerCast(secondOperand, int32Ty, false, "", loadGlobal->getNextNode());
+
+                    BinaryOperator *intermediateHash = generateHashFunction(&builder, firstOperand, loadGlobal);
+                    BinaryOperator *hash = generateHashFunction(&builder, secondOperand, intermediateHash);
+                    StoreInst *storeGlobal = builder.CreateStore(hash, globals[globalsIndex]);
+
+                    if (!finalRun) {
+                        instToObfuscate.push_back(intermediateHash);
+                        instToObfuscate.push_back(hash);
+                        instToObfuscate.push_back(loadGlobal);
+                        instToObfuscate.push_back(storeGlobal);
+                    }
+
+                    break;
+                }
+
+                default:
+                    break;
+            }
+
+            globalsIndex++;
+            if (globalsIndex >= globals.size()) {
+                globalsIndex = 0;
+
+                std::shuffle(globals.begin(), globals.end(), twister);
+            }
+        }
+    }
+
+    void OHProtectorPass::insertGlobals(Module &M, int numHashVars) {
+        for (int i = 0; i < numHashVars; i++) {
+            GlobalVariable *global = new GlobalVariable(M, int32Ty, false, GlobalValue::CommonLinkage, 0, "veryglobalmuchsecure");
+            global->setAlignment(4);
+
+            // Constant Definitions
+            ConstantInt *constInt = ConstantInt::get(M.getContext(), APInt(32, 0));
+
+            // Global Variable Definitions
+            global->setInitializer(constInt);
+
+            globals.push_back(global);
+        }
     }
 }
 
