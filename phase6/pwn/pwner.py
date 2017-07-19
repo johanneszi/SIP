e6fc1b7b75d81f509a2014140df4f4c5e6be3905
Zhechko Zhechev <jechkoj@gmail.com>
2017-07-19 22:41:35 +0200

    refactors pwner script

--- phase6/pwn/pwner.py
+++ phase6/pwn/pwner.py
@@ -1,8 +1,7 @@
 from sys import argv
-from struct import pack, unpack
 import r2pipe
 
-if len(argv) != 2:
+if len(argv) < 2:
     print('Please provide program name and output file!')
     exit(1)
 
@@ -17,23 +16,6 @@ def nops(start, n):
 
 def nopInst(addr):
     r2.cmd('wao nop @ ' + str(addr))
-
-class Instruction:
-    def __init__(self, address=None):
-        if address:
-            seek(address)
-        instruction = r2.cmdj('pdj 1')
-
-        instruction = instruction[0]
-        self.type = instruction['type'] if instruction.get('type') else None
-        self.size = instruction['size'] if instruction.get('size') else None
-        self.jump = instruction['jump'] if instruction.get('jump') else None
-
-def findPrintf():
-    for symbol in r2.cmdj('isj'):
-        if 'imp.printf' in symbol['name']:
-            return symbol['vaddr']
-    raise Exception("Printf not found!")
 
 if __name__ == "__main__":
 
@@ -66,11 +48,9 @@ if __name__ == "__main__":
     # accumolator for the result, with itself, so 0 is returned
     address = 0x00755d56
     seek(address)
-    r2.cmd('wx 0x4401fb') # add ebx, r15d 
-    seek(address+3)
-    r2.cmd('wx 0x4401f3') # add ebx, r14d
-    seek(address+6)
-    r2.cmd('wx 0x0fafdb') # imul ebx, ebx ; for fun and profit
+    r2.cmd('wxs 0x4401fb') # add ebx, r15d 
+    r2.cmd('wxs 0x4401f3') # add ebx, r14d
+    r2.cmd('wxs 0x0fafdb') # imul ebx, ebx ; for fun and profit
     address += 9
     nops(address, 9) # nop the remaining bytes
 
@@ -81,28 +61,33 @@ if __name__ == "__main__":
     # Insert only $ in setup_level
     nopInst(0x00716b1b) # deletes jmp to * inserter
     
-    # Change all results for RC for usec_delay
-    r2.cmd('wao nop @ 0x0070e70b')
-    r2.cmd('wx 0x40b701 @ 0x0070e70b')
-    
-    r2.cmd('wao nop @ 0x00723bae')
-    r2.cmd('wx 0x40b701 @ 0x00723bae')
+    if len(argv) > 2:
+        # Change all results for RC for usec_delay
+        nopInst(0x0070e70b)
+        r2.cmd('wx 0x40b701 @ 0x0070e70b')
 
-    r2.cmd('wao nop @ 0x0075053f')
-    r2.cmd('wx 0x40b701 @ 0x0075053f')
+        nopInst(0x00723bae)
+        r2.cmd('wx 0x40b701 @ 0x00723bae')
 
-    r2.cmd('wao nop @ 0x00754b7d')
-    r2.cmd('wx 0x40b701 @ 0x00754b7d')
+        nopInst(0x0075053f)
+        r2.cmd('wx 0x40b701 @ 0x0075053f')
+
+        nopInst(0x00754b7d)
+        r2.cmd('wx 0x40b701 @ 0x00754b7d')
+
+        nopInst(0x00769413)
+        r2.cmd('wx 0x40b701 @ 0x00769413')
+
+        # Nop all ebx involving instructions in usec_delay
+        nopInst(0x007531e3)
+        nopInst(0x007531e6)
+        nopInst(0x007531ed)
     
-    r2.cmd('wao nop @ 0x00769413')
-    r2.cmd('wx 0x40b701 @ 0x00769413')
+        if argv[2] == '0':
+            r2.cmd('wa mov ebx, 0xaae60 @ 0x007531e6') # make it slow
+        else:
+            r2.cmd('wa mov ebx, 0x100 @ 0x007531e6') # make it fast
     
-    # Nop all ebx involving instructions in usec_delay
-    nopInst(0x007531e3)
-    nopInst(0x007531e6)
-    nopInst(0x007531ed)
-    #r2.cmd('wa mov ebx, 0xaae60 @ 0x007531e6') # make it slow
-    r2.cmd('wa mov ebx, 0x100 @ 0x007531e6') # make it fast
     print('Done')
     
     r2.quit()
